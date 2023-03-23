c8d48e8b674e425edddd15315f3830dc8496d6c1
codedestroyer19 <mikekiwalabye@gmail.com>
2023-03-23 18:17:48 +0200

    More optimizations

--- components/Navigation/Footer.tsx
+++ components/Navigation/Footer.tsx
@@ -7,18 +7,16 @@ const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
       <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Market
-        </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Exchange
-        </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Tutorials
-        </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Wallets
-        </p>
+        {["Road Map", "Updates", "Tokenomics", "White Paper"].map(
+          (item, index) => (
+            <p
+              className="text-white text-base text-center mx-2 cursor-pointer"
+              key={`Item` + index}
+            >
+              {item}
+            </p>
+          )
+        )}
       </div>
       <div className="flex flex-[0.5] justify-center items-center">
         <Image src={logo} alt="logo" className="w-32" />
